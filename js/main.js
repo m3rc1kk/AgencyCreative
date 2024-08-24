6ec1d24dbee09d23c0424734fb9495edaf075ac3
m3rc1k <maksimkakcmblcl@gmail.com>
2024-08-24 19:17:33 +0300

    services(w Adapt)

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ const changeLightColorTeamIcon = (element) => {
 
 teamButton.addEventListener('mouseover', changeDarkColorTeamIcon);
 teamButton.addEventListener('mouseout', changeLightColorTeamIcon);
+
+
+const servicesLink = document.querySelector('.services__body-link');
+const servicesIcon = document.querySelector('.services__body-link-icon');
+
+const changeBlueColorServicesIcon = () => {
+    servicesIcon.src = './images/services/arrow-right-hover.svg';
+}
+
+const changeDarkColorServicesIcon = () => {
+    servicesIcon.src = './images/services/arrow-right.svg';
+}
+
+servicesLink.addEventListener('mouseover', changeBlueColorServicesIcon);
+servicesLink.addEventListener('mouseout', changeDarkColorServicesIcon);
